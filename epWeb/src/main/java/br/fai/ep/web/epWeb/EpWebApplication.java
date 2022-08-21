290b127a1dd24553f0f8e36c67f8116b434e2256
Lucas Lima <lucaslima@fai-mg.br>
2022-08-21 14:10:33 -0300

    feat: criado estrutura spring da parte Web.

---
+++ epWeb/src/main/java/br/fai/ep/web/epWeb/EpWebApplication.java
@@ -0,0 +1,13 @@
+package br.fai.ep.web.epWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpWebApplication.class, args);
+	}
+
+}
