0142f71596d6fd7c94993f9bad717b9ed0d8ef1d
Lucas Lima <lucaslima@fai-mg.br>
2022-09-04 17:52:33 -0300

    feat: implementado cadastrado de usuarios.

--- epWeb/src/main/java/br/fai/ep/epWeb/controller/AccountController.java
+++ epWeb/src/main/java/br/fai/ep/epWeb/controller/AccountController.java
@@ -1,10 +1,17 @@
 package br.fai.ep.epWeb.controller;
 
+import br.fai.ep.epEntities.Usuario;
+import br.fai.ep.epWeb.service.ServiceInterface;
+import br.fai.ep.epWeb.service.impl.UsuarioServiceImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AccountController {
+    private final ServiceInterface service = new UsuarioServiceImpl();
 
     @GetMapping("/account/authenticate")
     public String getAuthenticatePage() {
@@ -12,8 +19,18 @@ public class AccountController {
     }
 
     @GetMapping("/account/register")
-    public String getRegisterPage() {
+    public String getRegisterPage(final Model model, final Usuario user) {
         return "conta/register";
+    }
+
+    @PostMapping("/account/create")
+    private String createUser(final Usuario user, final Model model) {
+        final long newIdUser = service.create(user);
+        if (newIdUser == -1) {
+            return "redirect:/account/register";
+        }
+
+        return "redirect:/user/profile/" + newIdUser;
     }
 
     @GetMapping("/account/forgot-my-password")
@@ -35,5 +52,4 @@ public class AccountController {
     public String getUserEditPage() {
         return "/usuario/editar_perfil";
     }
-
 }
