10d5cfe8a5003ed3c3baeb5386367d3d206ad54e
Lucas Lima <lucaslima@fai-mg.br>
2022-09-10 22:50:18 -0300

    feat: implementado alteracao dos dados do usuario.

--- epWeb/src/main/java/br/fai/ep/epWeb/controller/UserController.java
+++ epWeb/src/main/java/br/fai/ep/epWeb/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -15,9 +16,15 @@ public class UserController {
     private final String MY_USER_REFERENCE = "myUser";
     private final String USER_CREATION_DATE = "dateCreate";
     private final String USER_ID = "userId";
+    private final String DATA_UPDATE_ERROR = "updateDataError";
 
+    private boolean triedPasswordChange = false;
+    private boolean updateUserDataError = false;
+
+    private Usuario temporaryUser = null;
 
     @GetMapping("/user/profile/{id}")
+
     public String getUserProfilePage(@PathVariable final long id, final Model model) {
         final Usuario user = (Usuario) service.readById(id);
         model.addAttribute(MY_USER_REFERENCE, user);
@@ -26,8 +33,51 @@ public class UserController {
         return "/usuario/perfil";
     }
 
-    @GetMapping("/user/edit")
-    public String getUserEditPage() {
-        return "/usuario/editar_perfil";
+    @GetMapping("/user/edit/{id}")
+    public String getUserEditPage(@PathVariable final int id, final Model model) {
+        try {
+            final Usuario user = (Usuario) service.readById(id);
+            if (user != null) {
+                triedPasswordChange = false;
+                temporaryUser = null;
+                model.addAttribute(user);
+                model.addAttribute(DATA_UPDATE_ERROR, updateUserDataError);
+                updateUserDataError = false;
+                return "/usuario/editar_perfil";
+            }
+
+            if (triedPasswordChange && temporaryUser != null) {
+                triedPasswordChange = false;
+                model.addAttribute(temporaryUser);
+                temporaryUser = null;
+
+                model.addAttribute(DATA_UPDATE_ERROR, updateUserDataError);
+                updateUserDataError = false;
+                return "/usuario/editar_perfil";
+            }
+
+            triedPasswordChange = false;
+            updateUserDataError = false;
+            return "redirect:/not-found";
+
+        } catch (final Exception ex) {
+            triedPasswordChange = false;
+            updateUserDataError = false;
+            return "redirect:/not-found";
+        }
+    }
+
+    @PostMapping("/user/update-user-data")
+    public String updateUserData(final Usuario user, final Model model) {
+        triedPasswordChange = true;
+        temporaryUser = user;
+
+        updateUserDataError = !service.update(user);
+        if (updateUserDataError) {
+            return "redirect:/user/edit/" + user.getId();
+        }
+        triedPasswordChange = false;
+        temporaryUser = null;
+        return "redirect:/user/profile/" + user.getId();
     }
 }
