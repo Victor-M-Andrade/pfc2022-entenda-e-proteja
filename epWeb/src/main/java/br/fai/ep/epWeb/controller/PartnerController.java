7309eca64871020f9ddf62c2b181d77665ddf2ca
Lucas Lima <lucaslima@fai-mg.br>
2022-09-20 23:01:41 -0300

    feat: implementado navegacao base para listar e ver os detalhes das entidades Parceiros

--- epWeb/src/main/java/br/fai/ep/epWeb/controller/PartnerController.java
+++ epWeb/src/main/java/br/fai/ep/epWeb/controller/PartnerController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PartnerController {
 
-    @GetMapping("/partners/list")
-    public String getPartnerIdPage() {
-        return "contatos/consultoria";
+    @GetMapping("/partner/list")
+    public String getPartnerListPage() {
+        return "parceiro/consultoria_list";
+    }
+
+    @GetMapping("/partner/detail")
+    public String getPartnerDetailPage() {
+        return "parceiro/consultoria_info";
     }
 }
