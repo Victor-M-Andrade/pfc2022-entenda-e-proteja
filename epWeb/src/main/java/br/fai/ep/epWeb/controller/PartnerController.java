6708f38aaf1d90794468e315e57292e31cfccaa9
Lucas Lima <lucaslima@fai-mg.br>
2022-09-30 22:29:59 -0300

    feat: implementado leitura de e edição de dados do parceiro consultor.

--- epWeb/src/main/java/br/fai/ep/epWeb/controller/PartnerController.java
+++ epWeb/src/main/java/br/fai/ep/epWeb/controller/PartnerController.java
@@ -22,10 +22,12 @@ public class PartnerController {
     private final String EXISTS_PARTNER = "existsPartner";
     private final String REGISTERED_PARTNER = "registeredPartner";
     private final String MY_PARTNER_REFERENCE = "myPartner";
+    private final String UPDATE_PARTNER_ERROR = "updatePartnerError";
     private final String ALREADY_REGISTERED_CNPJ = "alreadyRegisteredCnpj";
     private final String REGISTRATION_REQUEST_PROBLEMS = "registrationRequestProblems";
 
     private boolean alreadyRegisteredCnpj = false;
+    private boolean updatePartnerError = false;
     private boolean registrationRequestProblems = false;
 
     private Parceiro temporaryPartner = null;
@@ -91,7 +93,51 @@ public class PartnerController {
         }
 
         return "redirect:/partner/detail/" + newPartnerId;
-
     }
 
+    @GetMapping("/partner/my-data-as-partner/{id}")
+    public String getMyDataAsPartnerPage(@PathVariable final long id, final Model model) {
+        final Map<String, Long> map = new HashMap<>();
+        map.put(Parceiro.PARTNER_TABLE.ID_USER_COLUMN, id);
+        final List<Parceiro> partnerList = (List<Parceiro>) service.readByCriteria(map);
+        if (partnerList == null || partnerList.isEmpty() || partnerList.size() > 1) {
+            return "redirect:/not-found";
+        }
+
+        model.addAttribute(MY_PARTNER_REFERENCE, partnerList.get(0));
+        model.addAttribute(USER_ID, partnerList.get(0).getIdUsuario());
+        return "parceiro/perfil_consultor";
+    }
+
+    @GetMapping("/partner/edit-my-data-as-partner/{id}")
+    public String getEditMyDataAsPartnerPage(@PathVariable final long id, final Model model) {
+        Parceiro partner = (Parceiro) service.readById(id);
+        if (partner == null) {
+            return "redirect:/not-found";
+        }
+
+        model.addAttribute(UPDATE_PARTNER_ERROR, updatePartnerError);
+        if (updatePartnerError) {
+            updatePartnerError = false;
+        }
+
+        if (temporaryPartner != null) {
+            partner = temporaryPartner;
+            temporaryPartner = null;
+        }
+        model.addAttribute(MY_PARTNER_REFERENCE, partner);
+        model.addAttribute(USER_ID, partner.getIdUsuario());
+        return "parceiro/editar_parceiro";
+    }
+
+    @PostMapping("/partner/update-my-data-as-partner")
+    public String getEditMyDataAsPartnerPage(final Model model, final Parceiro partner) {
+        updatePartnerError = !service.update(partner);
+        if (updatePartnerError) {
+            temporaryPartner = partner;
+            return "redirect:/partner/edit-my-data-as-partner/" + partner.getId();
+        }
+
+        return "redirect:/partner/my-data-as-partner/" + partner.getIdUsuario();
+    }
 }
