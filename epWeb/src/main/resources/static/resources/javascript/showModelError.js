1a30ddad419b36633dc680d43acfb8300cf86dad
Lucas Lima <lucaslima@fai-mg.br>
2022-11-21 12:43:55 -0300

    refactor: adaptado funcao do dialog de erro para aparecer ou nao a mensagem das listas de problemas

--- epWeb/src/main/resources/static/resources/javascript/showModelError.js
+++ epWeb/src/main/resources/static/resources/javascript/showModelError.js
@@ -1,11 +1,11 @@
-function showModelError(message, title) {
+function showModelError(message, title, isProblemListField) {
     //set title message
     document.getElementById("errorModelLabel").innerHTML = title;
     
     //set error message
     message = message.replace(/\n/g , "<br>");
     document.getElementById("errorMessage").innerHTML = message;
-    
+    document.getElementById("showProblemListField").hidden = !isProblemListField;
     // show model
     $("#modelError").modal();
 }
